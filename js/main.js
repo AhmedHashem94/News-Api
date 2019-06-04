3310c928c58d114ecea1a92cb99746d965e4740f
Ahmed Hashem Elsayed <46004236+AhmedHashem94@users.noreply.github.com>
2019-06-04 13:03:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $("#search").click(function() {
       "get",
       "https://newsapi.org/v2/everything?q=" +
         news +
-        "&from=2019-04-30&sortBy=publishedAt&apiKey=c2a977e851cf42d3894be89afe141cce"
+        "&from=2019-06-30&sortBy=publishedAt&apiKey=c2a977e851cf42d3894be89afe141cce"
     );
     req.send();
     req.onload = function() {
